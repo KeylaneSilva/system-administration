d9f4e3435669b6ac670851a103ae1483f72c78b6
Keylane Silva <keylanessilva07@gmail.com>
2019-05-15 21:10:28 -0300

    sales

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Criando nossos Users --- OBS: Depois que adicionarmos o devise precisamos incluir o email e senha dos users
+User.create name: 'José', status: :active, kind: :salesman
+User.create name: 'Marcos', status: :active, kind: :manager
+ 
+# Criando alguns produtos de exemplo
+Product.create name: 'Smartphone', description:'Um smartphone novo ...', status: :active
+Product.create name: 'Tablet', description:'Um tablet novo ...', status: :active
+ 
+# Criando um desconto de exemplo
+Discount.create name: 'Desconto carnaval', description: 'Aplique esse desconto no carnaval', value: '10', kind: :porcent, status: :active
